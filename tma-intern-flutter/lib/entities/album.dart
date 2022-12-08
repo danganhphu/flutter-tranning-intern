af6ab6d9df9148059a418ba502441ce0895dfbb2
danganhphu <phudanganh107@gmail.com>
2022-12-08 17:58:24 +0700

    fix call API 08/12/2022

--- tma-intern-flutter/lib/entities/album.dart
+++ tma-intern-flutter/lib/entities/album.dart
@@ -2,36 +2,19 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Album {
-  int? id, userId;
-  String? title;
+  final int id, userId;
+  final String title;
 
-  Album({this.id, this.userId, this.title});
+  const Album({required this.id, required this.userId, required this.title});
 
   //chuyển đổi dữ liệu json sang obj
-  factory Album.fromJson(Map<String, dynamic> data) {
+  factory Album.fromJson(Map<String, dynamic> json) {
     return Album(
-      id: data["id"],
-      userId: data["userId"],
-      title: data["title"]
+      id: json["id"],
+      userId: json["userId"],
+      title: json["title"]
     );
   }
 
-  //lấy dữ liệu từ REST API
-  Future<List<Album>> getData() async {
-    const url = 'https://jsonplaceholder.typicode.com/albums';
-    final response = await http.get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      final json = jsonDecode(response.body) as List;
-      final albums = json.map((album) {
-        return Album(
-          id: album['id'],
-          userId: album['userId'],
-          title: album['title']
-        );
-      }).toList();
-      return albums;
-    }
-    throw Exception('Fail');
-  }
 
 }
--- tma-intern-flutter/lib/rest_api/album.dart
+++ tma-intern-flutter/lib/rest_api/album.dart
@@ -1,7 +1,22 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 import '../entities/album.dart';
+
+
+//lấy dữ liệu từ REST API
+Future<List<Album>> fetchAlbum() async {
+  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums'));
+  List<Album> albums ;
+  if (response.statusCode == 200) {
+    var list = json.decode(response.body) as List;
+    albums = list.map((item) => Album.fromJson(item)).toList();
+    return albums;
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
 class AlbumPage extends StatefulWidget {
   const AlbumPage({Key? key}) : super(key: key);
   final String title = "REST API Demo";
@@ -10,6 +25,14 @@ class AlbumPage extends StatefulWidget {
 }
 
 class _AlbumPageState extends State<AlbumPage> {
+  late Future<List<Album>> albums;
+
+  @override
+  void initState() {
+    super.initState();
+    albums = fetchAlbum();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,10 +40,12 @@ class _AlbumPageState extends State<AlbumPage> {
         title: Text(widget.title),
       ),
       //widget future
-      body: FutureBuilder(
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.hasError)
+      body: FutureBuilder<List<Album>>(
+        future: albums,
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.hasError) {
             print(snapshot.error);
+          }
           return snapshot.hasData ? AlbumList(albums: snapshot.data) : Center(child: CircularProgressIndicator());
         }
       ),
@@ -29,8 +54,9 @@ class _AlbumPageState extends State<AlbumPage> {
 }
 
 class AlbumList extends StatelessWidget {
+  List<Album> albums;
   AlbumList({Key? key, required this.albums,}) : super(key: key);
-  List<Album> albums = [];
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -38,7 +64,7 @@ class AlbumList extends StatelessWidget {
       itemBuilder: (context, index) {
         return GestureDetector(
           child: Container(
-            padding: EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
             color: index % 2 == 0 ? Colors.lightBlue : Colors.green,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
