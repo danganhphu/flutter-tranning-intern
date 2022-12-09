dc7213db39d5f4f86f36a358e7d80c1cf828c006
danganhphu <phudanganh107@gmail.com>
2022-12-09 09:04:17 +0700

    commit handle album api

---
+++ tma-intern-flutter/lib/apis/album_apis.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../entities/album.dart';
+
+class AlbumRestAPI {
+  String url = 'https://jsonplaceholder.typicode.com/albums';
+
+  //lấy dữ liệu từ REST API
+  Future<List<Album>> fetchAlbum() async {
+    final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums'));
+    List<Album> albums ;
+    if (response.statusCode == 200) {
+      List<dynamic> body = jsonDecode(response.body);
+      albums = body.map((dynamic album) => Album.fromJson(album)).toList();
+      for (var album in albums) {
+        print(album);
+      }
+      return albums;
+    } else {
+      print('không lấy được albums');
+      throw Exception('Failed to load album');
+    }
+  }
+}
