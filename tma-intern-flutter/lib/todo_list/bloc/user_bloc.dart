5fff55976105cd3341268f50b6bf70566a2c9a46
danganhphu <phudanganh107@gmail.com>
2022-12-26 09:50:44 +0700

    add func insert, delete, select

--- tma-intern-flutter/lib/todo_list/bloc/user_bloc.dart
+++ tma-intern-flutter/lib/todo_list/bloc/user_bloc.dart
@@ -4,20 +4,39 @@ import 'dart:math';
 import '../base/base.dart';
 import '../models/user.dart';
 import '../events/events.dart';
+import 'package:flutterproject/todo_list/database/database.dart';
 
 class UserBloc extends BaseBloc {
+  DatabaseTable _databaseTable = DatabaseTable();
+
   StreamController<List<User>> _userListStreamCtrl = StreamController<List<User>>();
   Stream<List<User>> get userListStream => _userListStreamCtrl.stream;
 
   final _randomInt =  Random();
 
-  final List<User> _userList = [];
-  _addUser(User user) {
+  List<User> _userList = [];
+
+  initData() async {
+    _userList = await _databaseTable.selectAllUsers();
+    if(_userList == null) {
+      return;
+    }
+    _userListStreamCtrl.sink.add(_userList);
+  }
+
+  _addUser(User user) async {
+    //thêm vào database
+   await _databaseTable.insertUser(user);
+
+
     _userList.add(user);
     _userListStreamCtrl.sink.add(_userList);
   }
 
-  _deleteUser(User user) {
+  _deleteUser(User user) async {
+    //xoa user
+    await _databaseTable.deleteUser(user);
+
     _userList.remove(user);
     _userListStreamCtrl.sink.add(_userList);
   }
@@ -27,10 +46,10 @@ class UserBloc extends BaseBloc {
     if (event is AddUserEvent) {
       User user = User(_randomInt.nextInt(10000), event.name);
       _addUser(user);
-      // print(event.name);
+      print('add: ${event.name}');
     } else if (event is DeleteUserEvent) {
       _deleteUser(event.user);
-      print('delete event');
+      print('delete: ${event.user.name}');
     }
   }
   @override
