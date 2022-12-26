1acc37f27df7910e102dce9006588d163d05e0f4
danganhphu <phudanganh107@gmail.com>
2022-12-26 16:32:25 +0700

    test api pokemon

--- tma-intern-flutter/lib/test/test.dart
+++ tma-intern-flutter/lib/test/test.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
@@ -9,7 +10,6 @@ void getResultAPI() async {
   const _BASE_URL = 'https://pokeapi.co/api/v2';
   var url = Uri.parse('${_BASE_URL}/pokemon');
   var response = await http.get(url);
-
   if (response.statusCode == 200) {
     var json = jsonDecode(response.body);
     for (var item in json['results']) {
