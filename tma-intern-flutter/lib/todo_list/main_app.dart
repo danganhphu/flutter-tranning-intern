2af801d58ce1a54f469fc6ad3f65d3206ab16e1e
danganhphu <phudanganh107@gmail.com>
2022-12-26 09:52:14 +0700

    fix init database

--- tma-intern-flutter/lib/todo_list/main_app.dart
+++ tma-intern-flutter/lib/todo_list/main_app.dart
@@ -4,6 +4,9 @@ import 'package:flutterproject/todo_list/screens/screens.dart';
 import 'package:flutterproject/todo_list/database/database.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await DatabaseHelper().initDb();
-  runApp(const MyAppUser());
+  runApp(
+      const MyAppUser()
+  );
 }
